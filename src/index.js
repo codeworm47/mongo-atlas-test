c2c31b9cb456b0b51080b3ef7b3c51753b7a039f
hesamr asouli <hesam.rasouli.teherani@schibsted.com>
2022-12-26 04:01:17 +0100

    added files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
     const { MongoClient, ServerApiVersion } = require('mongodb');
     const uri = process.env.MONGO_URI;
     console.log("uri::", uri);
-    const config = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1, connectTimeoutMS: 500, };
+    const config = { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1, connectTimeoutMS: 15000, };
     const client = new MongoClient(uri, config);
     await client.connect().then(()=> {
         console.log("SUCCESSFULLY CONNECTED");
